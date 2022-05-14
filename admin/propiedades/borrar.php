4a4dc5c386bfd3a384b2e223ca5b425a69ca3271
JmcaDev <jmcavzla@gmail.com>
2022-05-14 17:13:49 -0400

    Se agrega el Proyecto

---
+++ admin/propiedades/borrar.php
@@ -0,0 +1,13 @@
+<?php 
+    require '../../includes/funciones.php';
+    
+    incluirTemplate('auxiliarHeader'); 
+?>
+
+  <main class="contenedor seccion">
+    <h1>Borrar</h1>
+  </main>
+
+<?php
+        incluirTemplate('auxiliarFooter'); 
+?>
